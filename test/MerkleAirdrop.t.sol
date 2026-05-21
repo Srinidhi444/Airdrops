a932d746f0ba458fb0bc5c21ff820049b05edcc9
Srinidhi-4419 <kulkarnisrinidhi85@gmail.com>
2026-05-21 12:12:28 +0530

    added scripts for generting inputs and output json used for testing later

---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {Test} from "forge-std/Test.sol";
+
+contract MerkleAirdropTest is Test {
+    function setUp() public {
+        // Set up your test environment here
+    }
+
+    function testClaim() public {
+        // Write your test for the claim function here
+    }
+}
